6d86980015888746d1ecd1f2e698b8db938978cb
Fabian Thies <git@fabian-thies.de>
2022-11-28 23:53:30 +0100

    Switch permission icons to filled versions for a more cohesive look

--- AltStoreCore/Model/AppPermission.swift
+++ AltStoreCore/Model/AppPermission.swift
@@ -49,23 +49,24 @@ public extension ALTAppPermissionType
         switch self
         {
         case .photos: return UIImage(systemName: "photo.on.rectangle.angled")
-        case .camera: return UIImage(systemName: "camera")
-        case .location: return UIImage(systemName: "location")
-        case .contacts: return UIImage(systemName: "person.2")
+        case .camera: return UIImage(systemName: "camera.fill")
+        case .location: return UIImage(systemName: "location.fill")
+        case .contacts: return UIImage(systemName: "person.2.fill")
         case .reminders: return UIImage(systemName: "checklist")
         case .appleMusic: return UIImage(systemName: "music.note")
-        case .microphone: return UIImage(systemName: "mic")
+        case .microphone: return UIImage(systemName: "mic.fill")
         case .speechRecognition: return UIImage(systemName: "waveform.and.mic")
-        case .backgroundAudio: return UIImage(systemName: "speaker")
+        case .backgroundAudio: return UIImage(systemName: "speaker.fill")
         case .backgroundFetch: return UIImage(systemName: "square.and.arrow.down")
-        case .bluetooth: return UIImage(systemName: "wave.3.right") // TODO: Replace with bluetooth icon
+        case .bluetooth: return UIImage(systemName: "wave.3.right")
         case .network: return UIImage(systemName: "network")
         case .calendars: return UIImage(systemName: "calendar")
         case .touchID: return UIImage(systemName: "touchid")
         case .faceID: return UIImage(systemName: "faceid")
-        case .siri: return UIImage(systemName: "mic.and.signal.meter") // TODO: Replace with siri icon
+        case .siri: return UIImage(systemName: "mic.and.signal.meter.fill")
         case .motion: return UIImage(systemName: "figure.walk.motion")
-        default: return nil
+        default:
+            return nil
         }
     }
 }
